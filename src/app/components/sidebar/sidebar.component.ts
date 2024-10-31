3f6eb8e9f10b3e5ee91980c65308fe927a58c53e
Joshua Pedraza <luisangelticona575@gmail.com>
2024-10-30 19:17:04 -0500

    Descripción de los cambios que realizaste

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isSidebarOpen = true;
 
+  constructor(private router: Router) {}
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
+  }
 }
